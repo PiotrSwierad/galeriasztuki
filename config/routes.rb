63e01c868da652969c0a63f657944250200a1834
Piotr Świerad <piotrswierad.mail@gmail.com>
2018-10-23 15:38:27 +0200

    WIP logowanie

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'session/new'
+
   root 'home#index'
 
   get '/about', to: 'home#about'
@@ -10,8 +12,13 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
 
-  resources :users
+  get '/login', to: 'session#new'
 
+  post '/login', to: 'session#create'
+
+  delete '/login', to: 'session#destroy'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
